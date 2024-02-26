253515ad8c88c58d0e3093d090603ba0abcc9bd5
moshawa <moshawa0@gmail.com>
2024-02-25 20:12:45 -0500

    setup deploy pipeline

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	root: 'src/',
 	publicDir: '../static/',
-	base: './',
+	base: '/ps-flag/',
 	server: {
 		host: true, // Open to local network and display URL
 		open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
